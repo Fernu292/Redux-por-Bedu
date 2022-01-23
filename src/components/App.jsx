f25267f89bea1afc087efaa1e382400792041fc0
Luis Fernando <fernu292@gmail.com>
2022-01-22 19:15:22 -0600

    Agregando configuracion dist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const App = () => {
-  return <h1>Blog Platzi</h1>;
+  return (
+    <>
+      <h1>Blog Platzi</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Enlace</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Fernando</td>
+            <td>fernu@gmail.com</td>
+            <td>fernu.com</td>
+          </tr>
+          <tr>
+            <td>Maosin</td>
+            <td>feto@gmail.com</td>
+            <td>fetus.com</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -25,6 +27,14 @@ module.exports = {
           loader: "html-loader",
         },
       },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          "style-loader",
+          "css-loader",
+        ],
+      },
     ],
   },
   plugins: [
@@ -33,6 +43,10 @@ module.exports = {
       inject: true,
       filename: "index.html",
     }),
+    new MiniCssExtractPlugin({
+      filename: "assests/[name][contenthash].css",
+    }),
+    new CleanWebpackPlugin(),
   ],
   devServer: {
     static: {
